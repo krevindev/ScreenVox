3892fbc2213a498d28a7be2204ef436798a6d932
Kyle Revin <kylitadavinci@gmail.com>
2023-07-23 16:16:40 +0800

    First Commit

--- src/pages/home/signin/SignInPage.js
+++ src/pages/home/signin/SignInPage.js
@@ -1,8 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import { useState, useRef, useEffect } from 'react';
 import './SignInPage.css';
 
 export default function SignInPage() {
+
+    const navigate = useNavigate();
+
+    const [isSignUp, setIsSignUp] = useState(false);
+
+    const pageRef = useRef(null);
+
+    useEffect(() => {
+        if (isSignUp) {
+            pageRef.current.classList.add('reg-form');
+        }else{
+            pageRef.current.classList.remove('reg-form')
+        }
+    })
+
     return (
-        <div id='SignInPage' className='route-page'>
+        <div id='SignInPage' className='route-page' ref={pageRef}>
             <div id='signin-page-head'>
                 <div id='signin-page-head-content'>
                     <div id='signin-page-head-content-inner'>
@@ -15,7 +32,8 @@ export default function SignInPage() {
                     <form onSubmit={e => e.preventDefault()}>
                         <input placeholder='Username' required />
                         <input type='password' placeholder='Password' required />
-                        <button type='submit'>SIGN IN</button>
+                        <button className='form-submit-btn' type='submit'>SIGN IN</button>
+                        <button onClick={e => setIsSignUp(true)} >SIGN UP</button>
                     </form>
                 </div>
             </div>
